06463d9d7a64e818d5b9530185e4b0a15e562eb9
Pervej0 <mdparvez222khan@gmail.com>
2024-01-02 19:31:32 +0600

    update enrolledCourse done

---
+++ src/app/modules/enrolledCourse/enrolledCourse.utils.ts
@@ -0,0 +1,53 @@
+export const CountGradeAndPoints = (courseMarks: Record<string, number>) => {
+  const {
+    classTest1 = 0,
+    midTerm = 0,
+    classTest2 = 0,
+    finalTerm = 0,
+  } = courseMarks
+  const totalMarks = Math.ceil(
+    classTest1 * 0.1 + midTerm * 0.3 + classTest2 * 0.1 + finalTerm * 0.5,
+  )
+
+  /**
+   * 0-19 F
+   * 20-39 D
+   * 40-59 C
+   * 60-79 B
+   * 80-100 A
+   */
+  let result = {
+    grade: 'NA',
+    gradePoints: 0,
+  }
+  //   console.log(totalMarks)
+
+  if (totalMarks > 0 && totalMarks <= 30) {
+    result = {
+      grade: 'F',
+      gradePoints: 0.0,
+    }
+  } else if (totalMarks >= 40 && totalMarks <= 59) {
+    result = {
+      grade: 'C',
+      gradePoints: 3.0,
+    }
+  } else if (totalMarks >= 60 && totalMarks <= 79) {
+    result = {
+      grade: 'B',
+      gradePoints: 3.5,
+    }
+  } else if (totalMarks >= 80 && totalMarks <= 100) {
+    result = {
+      grade: 'A',
+      gradePoints: 4.0,
+    }
+  } else {
+    result = {
+      grade: 'NA',
+      gradePoints: 0,
+    }
+  }
+
+  return result
+}
